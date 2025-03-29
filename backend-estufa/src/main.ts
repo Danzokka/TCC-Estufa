a49a1923c9263368ffc6b9fc90b05b2db1667384
Rafael Dantas Boeira <rafaeldantasboeira@gmail.com>
2025-03-29 15:07:44 -0300

    inicializando swagger

--- backend-estufa/src/main.ts
+++ backend-estufa/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const config = new DocumentBuilder()
+    .setTitle('Estufa API')
+    .setDescription('API for Estufa')
+    .setVersion('1.0')
+    .addTag('estufa')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory());
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
