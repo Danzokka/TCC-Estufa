6283e50d25859b25975a24321aa0ddfe765bdf38
Rafael Dantas Boeira <rafaeldantasboeira@gmail.com>
2025-04-18 16:11:28 -0300

    Init turborepo

---
+++ apps/api/src/sensor/dto/CreateSensorDataDto.ts
@@ -0,0 +1,9 @@
+export class CreateSensorDataDto {
+  air_temperature: number;
+  air_humidity: number;
+  soil_temperature: number;
+  soil_moisture: number;
+  light_intensity: number;
+  water_level: number;
+  water_reserve: number;  
+}
---
+++ apps/api/src/sensor/sensor.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { SensorService } from './sensor.service';
+import { CreateSensorDataDto } from './dto/CreateSensorDataDto';
+
+@Controller('sensor')
+export class SensorController {
+  constructor(private readonly sensorService: SensorService) {}
+
+  @Post()
+  sendData(@Body() data: CreateSensorDataDto) {
+    try {
+      this.sensorService.sendData(data);
+      return { message: 'Data sent successfully' };
+    } catch (error) {
+      console.error('Error sending data:', error);
+      return { message: 'Error sending data', error: error.message };
+    }
+  }
+
+}
