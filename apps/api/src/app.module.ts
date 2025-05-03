cd4d879d461d7ca2d802231cab475936d8710fe5
Rafael Dantas <rafaeldantasboeira@gmail.com>
2025-05-03 15:25:39 -0300

    Inicializando Modulos de Usuario, Blog e Planta

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,9 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SensorModule } from './sensor/sensor.module';
 import { PrismaService } from './prisma.service';
+import { UserModule } from './user/user.module';
+import { BlogModule } from './blog/blog.module';
+import { PlantModule } from './plant/plant.module';
 
 @Module({
-  imports: [SensorModule],
+  imports: [SensorModule, UserModule, BlogModule, PlantModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
